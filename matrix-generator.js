ff2a36f0056539f5bdb4e909a7b61d99983cf6f6
kmietotwy <kmietotwy@gmail.com>
2019-04-10 09:41:00 -1000

    matrix generator

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,40 @@
 function matrixGenerator(rows, columns) {
   // do work here
+  let result = [];
+
+  //Generate random arrays and sort before pushing into result
+  for (let i = 0; i < rows; i++) {
+    let newArr = [];
+
+    for (let j = 0; j < columns; j++) {
+      let randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+      newArr.push(randomNumber);
+    }
+
+    result.push(newArr.sort());
+  }
+
+  //Sort arrays in result by first index
+  for (let k = 0; k < result.length; k++) {
+    let smallestIndex = k;
+    let smallestElement = result[k][0];
+
+    for (let m = k + 1; m < result.length; m++) {
+      if (result[m][0] < smallestElement) {
+        smallestIndex = m;
+        smallestElement = result[m][0];
+      };
+    }
+
+    if (smallestIndex !== k) {
+      let temp = result[k];
+      result[k] = result[smallestIndex];
+      result[smallestIndex] = temp;
+    };
+
+  }
+
+  return result;
 }
 
 module.exports = matrixGenerator;
