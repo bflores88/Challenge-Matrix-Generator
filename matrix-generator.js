3056628dacf1f7a1d595e255b0a219c7222b5d9e
kmietotwy <kmietotwy@gmail.com>
2019-04-10 11:14:33 -1000

    refactored using fat arrow compare sort

--- matrix-generator.js
+++ matrix-generator.js
@@ -4,38 +4,20 @@ function matrixGenerator(rows, columns) {
   // do work here
   let result = [];
 
-  //Generate random arrays and sort before pushing into result
   for (let i = 0; i < rows; i++) {
     let newArr = [];
 
     for (let j = 0; j < columns; j++) {
-      let randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+      let randomNumber = Math.floor(Math.random() * 100) + 1;
       newArr.push(randomNumber);
     };
 
-    result.push(newArr.sort());
+    let sortArr = newArr.sort((a,b) => a-b);
+    result.push(sortArr);
   };
 
-  //Sort arrays in result by first index using selection sort
-  for (let k = 0; k < result.length; k++) {
-    let smallestIndex = k;
-    let smallestElement = result[k][0];
-
-    for (let m = k + 1; m < result.length; m++) {
-      if (result[m][0] < smallestElement) {
-        smallestIndex = m;
-        smallestElement = result[m][0];
-      };
-    };
-
-    if (smallestIndex !== k) {
-      let temp = result[k];
-      result[k] = result[smallestIndex];
-      result[smallestIndex] = temp;
-    };
-  };
-
-  return result;
+  let sortResult = result.sort((a,b) => a[0] - b[0]);
+  return sortResult;
 }
 
 module.exports = matrixGenerator;
